eb173eedbba02888af14cf6a4915163d8116d996
Julian Stephens <julian@julianstephens.net>
2025-07-26 23:23:39 -0400

    feat: add job execution table entry to worker

--- services/workersvc/main.go
+++ services/workersvc/main.go
@@ -4,16 +4,23 @@ import (
 	"encoding/json"
 	"time"
 
+	"github.com/google/uuid"
 	"github.com/julianstephens/distributed-job-manager/pkg/config"
 	"github.com/julianstephens/distributed-job-manager/pkg/logger"
 	"github.com/julianstephens/distributed-job-manager/pkg/models"
 	"github.com/julianstephens/distributed-job-manager/pkg/queue"
+	"github.com/julianstephens/distributed-job-manager/pkg/store"
 	"github.com/julianstephens/distributed-job-manager/services/workersvc/worker"
 	"github.com/rabbitmq/amqp091-go"
 )
 
 func main() {
 	conf := config.GetConfig()
+	db, err := store.GetDB(conf.Cassandra.Keyspace)
+	if err != nil {
+		logger.Fatalf("unable to get db connection: %v", err)
+		return
+	}
 	conn, ch, err := queue.GetConnection(conf)
 	defer queue.CloseConnection(conn, ch)
 
@@ -43,14 +50,19 @@ func main() {
 	runner := worker.NewRunner()
 
 	var forever chan struct{}
-	go processJobs(runner, pool, msgs)
+	go processJobs(conf, db, runner, pool, msgs)
 	<-forever
 }
 
-func processJobs(runner *worker.Runner, pool *worker.SandboxPool, msgs <-chan amqp091.Delivery) {
+func processJobs(config *models.Config, db *store.DBSession, runner *worker.Runner, pool *worker.SandboxPool, msgs <-chan amqp091.Delivery) {
 	var job models.Job
 	for d := range msgs {
 		if err := json.Unmarshal(d.Body, &job); err != nil {
+			panic(err)
+		}
+
+		err := createJobExecutionEntry(job, config, db)
+		if err != nil {
 			panic(err)
 		}
 
@@ -75,4 +87,20 @@ func processJobs(runner *worker.Runner, pool *worker.SandboxPool, msgs <-chan am
 		logger.Infof("user %s released sandbox %d", job.UserID, box.ID)
 		logger.Infof("%d sandboxes available", pool.AvailableCount())
 	}
+}
+
+func createJobExecutionEntry(job models.Job, config *models.Config, db *store.DBSession) error {
+	jobExecution := models.JobExecution{
+		ExecutionID: uuid.NewString(),
+		WorkerID:    config.WorkerID,
+		JobID:       job.JobID,
+		Status:      "running",
+	}
+
+	q := db.Client.Query(models.JobExecutions.Insert()).BindStruct(jobExecution)
+	if err := q.ExecRelease(); err != nil {
+		return err
+	}
+
+	return nil
 }
